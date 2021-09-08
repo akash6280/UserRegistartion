649bdf75b9cf7ee298374a7207c4bc8ac4b94e29
Akash Singh <akashsingh6280@gmail.com>
2021-09-09 03:26:44 +0530

    ADDED: function to verify list of email id

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -58,13 +58,28 @@ public class UserRegistration {
 		System.out.println("Enter password");
 		String password=scanner.nextLine();
 		
-		boolean result = Pattern.matches("(?=.*[A-Z])(?=.*?[0-9])(?=.*[#?!@$%^&*-])(?=.*[#?!@$%^&*-]).{8,}$", password);
+		boolean result = Pattern.matches("(?=.*?[A-Z])(?=.*?[0-9])(?=.*[#?!@$%^&*-]).{8,}$", password);
 		if(result) {
 			System.out.println("Valid");
 		}
 		else {
 			System.out.println("Invalid");
 		}
+	}
+	
+	public static void verifyEmailList() {
+		String[] emailId= {"abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com",
+				"abc@gmail.com.com","abc+100@gmail.com","abc","abc@.com.my","abc123@gmail.a","abc123@.com","abc123@.com.com",".abc@abc.com","abc()*@gmail.com","abc@%*.com",
+				"abc..2002@gmail.com","abc.@gmail.com","abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au"};
+		for(int i=0;i<emailId.length;i++) {	
+			boolean result = Pattern.matches("^[a-zA-Z][a-zA-Z0-9]*[a-zA-Z0-9]([+-_.][a-zA-Z]*)?@[a-zA-Z0-9]*[.][a-z]{2,4}([.][a-zA-z]{2})?$", emailId[i]);
+			if(result) {
+				System.out.println("Valid"); 
+			}
+			else {
+				System.out.println("Invalid"); 
+			}
+		}		
 	}
 	public static void main(String[] args) {
 		
@@ -73,6 +88,7 @@ public class UserRegistration {
 		validateEmail();
 		validatePhoneNumber();
 		validatePassword();
+		verifyEmailList();
 	}
 }
 		
